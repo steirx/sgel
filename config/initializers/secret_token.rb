45822f5d07ea7005d541f60a442f72ff2d1c8285
Eric <eric@ubuntu.ubuntu-domain>
2010-11-14 15:14:20 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sgel::Application.config.secret_token = '3eb96b9d81fdd388d2925ac8c8733e8a62703fade64fa18811908fd68d457f1e35e93e7c21f797ff3b9cbc752633bb8f5d365fcc082af3e60ed4607df1dffdc8'
